094a309fa63d881a9c2d05a349af2e95be9c8b91
N.U <n.n.ushakova@gmail.com>
2021-01-01 21:20:36 -0500

    Simplify the solution

--- Easy/PalindromInteger.py
+++ Easy/PalindromInteger.py
@@ -4,10 +4,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         self.x = x
-        # as '-' goes from the first to the last position, all negative numbers are NOT palindroms
-        if x < 0:
-            return False
-
+    
         x = str(x)        
         for i in range(0, len(x)//2):
             if x[i] != x[len(x)-1-i]: # compare 1st digit with the last, 2nd with (last-1) and so on until come to the middle      
